256ec7b811706d6e7d363bd674dcccc5e38e90be
Konstantin Agafonov <konstantin.agafonov@gmail.com>
2024-07-28 14:42:58 +0300

    Get country by phone number

--- app/Http/Controllers/GuestController.php
+++ app/Http/Controllers/GuestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Guest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class GuestController extends Controller
 {
@@ -21,12 +22,38 @@ class GuestController extends Controller
     public function store(Request $request)
     {
         $fields = $request->validate([
-            'name'      => ['required', 'max:50', 'min:2'],
-            'surname'   => ['required', 'max:50', 'min:2'],
-            'phone'     => ['required', 'max:20', 'min:3','unique:guests'],
-            'email'     => ['email','unique:guests'],
-            'country'   => ['max:2', 'min:2'],
+            'name' => ['required', 'max:50', 'min:2'],
+            'surname' => ['required', 'max:50', 'min:2'],
+            'phone' => ['required', 'max:20', 'min:3', 'unique:guests'],
+            'email' => ['email', 'unique:guests'],
+            'country' => ['max:2', 'min:2'],
         ]);
+
+        $country_is_valid = false;
+        $countries_data = Storage::json('CountryCodes.json');
+        if (! empty($fields['country'])) {
+            foreach ($countries_data as $country) {
+                if ($fields['country'] === $country['code']) {
+                    $country_is_valid = true;
+                    break;
+                }
+            }
+        } else {
+            $country_is_valid = true;
+
+            foreach ($countries_data as $country) {
+                if (str_starts_with($fields['phone'], $country['dial_code'])) {
+                    $fields['country'] = $country['code'];
+                    break;
+                }
+            }
+        }
+
+        if (! $country_is_valid) {
+            return [
+                'message' => 'Country is not valid',
+            ];
+        }
 
         $guest = Guest::create($fields);
 
@@ -51,11 +78,11 @@ class GuestController extends Controller
     public function update(Request $request, Guest $guest)
     {
         $fields = $request->validate([
-            'name'      => ['required', 'max:50', 'min:2'],
-            'surname'   => ['required', 'max:50', 'min:2'],
-            'phone'     => ['required', 'max:20', 'min:3','unique:guests'],
-            'email'     => ['email','unique:guests'],
-            'country'   => ['max:2', 'min:2'],
+            'name' => ['required', 'max:50', 'min:2'],
+            'surname' => ['required', 'max:50', 'min:2'],
+            'phone' => ['required', 'max:20', 'min:3', 'unique:guests'],
+            'email' => ['email', 'unique:guests'],
+            'country' => ['max:2', 'min:2'],
         ]);
 
         $guest->update($fields);
